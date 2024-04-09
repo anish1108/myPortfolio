cb7d57e9056606fb4aabeb35bac7d697111f7579
anish11 <anishprasad2512@gmail.com>
2024-04-10 00:07:53 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./public/*.html"],
   theme: {
     extend: {
-      
+      safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]']
     },
   },
   plugins: [],
